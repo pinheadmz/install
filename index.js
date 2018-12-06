a5d3384c9c55488bfb2239ddbe4415fd74003096
Matthew Zipkin <pinheadmz@gmail.com>
2018-12-06 18:02:21 +0000

    run local lib only

--- index.js
+++ index.js
@@ -249,7 +249,7 @@ let options = {};
   const prefix = '--prefix=' + Path.join(pathData, options.library);
   const spv = options.node === 'SPV' ? '--spv' : null;
   const libProc = child_process.spawn(
-    options.library,
+    './' + options.library,
     ['--daemon', spv, prefix],
     {
       cwd: Path.join(pathLibs, options.library, 'bin'),
