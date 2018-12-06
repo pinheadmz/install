142798b6fa616c393b529c424201bfb4730cd815
Matthew Zipkin <pinheadmz@gmail.com>
2018-12-06 14:18:19 -0800

    bash start command

---
+++ README.md
@@ -0,0 +1,5 @@
+Run this command in a terminal:
+
+```
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/pinheadmz/install/master/dist/start.sh)"
+```
---
+++ dist/start.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+curl -fsSL https://raw.githubusercontent.com/pinheadmz/install/master/dist/bundle.js > EZ_bcoin_installer.js
+node EZ_bcoin_installer.js
