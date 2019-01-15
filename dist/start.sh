7425956c6444f6ac9c82c389ab6ec3d2bb0632ed
Matthew Zipkin <pinheadmz@gmail.com>
2019-01-14 16:16:14 -0800

    rename installer

--- dist/start.sh
+++ dist/start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-curl -fsSL https://raw.githubusercontent.com/pinheadmz/install/master/dist/bundle.js > EZ_bcoin_installer.js
-node EZ_bcoin_installer.js
+curl -fsSL https://raw.githubusercontent.com/pinheadmz/install/master/dist/bundle.js > b-installer.js
+node b-installer.js
